fd62928c2795247a1e5fd89b8bff1366d8b147a2
Nguyen Duc Hieu <86180191+hieu26102001@users.noreply.github.com>
2022-03-09 09:46:40 +0700

    Update footer + header

--- components/partials/footer.jsx
+++ components/partials/footer.jsx
@@ -1,146 +1,147 @@
 import { Disclosure } from "@headlessui/react"
 import { useState } from "react"
 import { FooterNavigation, Media } from "../../data/globals"
-import {ChevronDownIcon} from '@heroicons/react/solid'
+import { ChevronDownIcon } from '@heroicons/react/solid'
 
 export default function Footer() {
     const { Active, setActive } = useState(0)
 
     return (
-        <footer className="mx-auto h-80 px-[20px] absolute my-8">
-            <div className="hidden md:grid grid-cols-12 gap-16 ">
-                <div className="col-span-4 space-y-2 ">
-                    <div className=" mb-6">
-                        <img src="/LOGO PAYMENT.png" />
-                    </div>
-                    <p className="text-justify font-normal text-xs leading-normal text-[#717171]">With Payment, we are bringing your favorite business to your Binance app. Go to Binance Marketplace with your existing Binance account and make a payment using Payment. Spending cryptocurrency has become a lot easier.</p>
-                </div>
-
-                <div className="md:flex mt-4 md:mt-0 col-span-6 md:space-x-2 space-y-4 md:space-y-0 justify-between">
-                    <div className="space-y-3 md:w-28">
-                        <h4 className="text-sm font-medium  mb-4">About us</h4>
-                        <ul className="space-y-2">
-                            {FooterNavigation["About us"].map((item) => (
-                                <li key={item.name} className="text-xs text-[#2F88FF]">
-                                    {item.name}
-                                </li>
-                            ))
-                            }
-                        </ul>
-                    </div>
-
-                    <div className="space-y-3 md:w-28">
-                        <h4 className="text-sm font-medium mb-4">Service</h4>
-                        <ul className="space-y-2">
-                            {FooterNavigation["Service"].map((item) => (
-                                <li key={item.name} className="text-xs text-[#2F88FF]">
-                                    {item.name}
-                                </li>
-                            ))
-                            }
-                        </ul>
-                    </div>
-
-                    <div className="space-y-3 md:w-32">
-                        <h4 className="text-sm font-medium  mb-4">Support</h4>
-                        <ul className="space-y-2">
-                            {FooterNavigation["Support"].map((item) => (
-                                <li key={item.name} className="text-xs text-[#2F88FF]">
-                                    {item.name}
-                                </li>
-                            ))
-                            }
-                        </ul>
-                    </div>
-
-                </div>
-            </div>
-            
-        {/* mobile */}
-            <div className="md:hidden">
-                <div className="col-span-4 mb-4 ">
-                    <div className=" mb-6">
-                        <img src="/LOGO PAYMENT.png" />
-                    </div>
-                    <p className="text-justify font-normal text-xs leading-normal text-[#717171]">With Payment, we are bringing your favorite business to your Binance app. Go to Binance Marketplace with your existing Binance account and make a payment using Payment. Spending cryptocurrency has become a lot easier.</p>
-                </div>
-                <Disclosure>
-                    {({ open }) => (
-                        <div className="my-3">
-                            <Disclosure.Button className="flex justify-between w-full">
-                                <h4 className="text-sm font-semibold" >About us</h4>
-                                <ChevronDownIcon width={16} className={`${open?"":"rotate-90"}`}/>
-                            </Disclosure.Button>
-                            <Disclosure.Panel>
-                                <ul className="space-y-2 py-4">
-                                    {FooterNavigation["About us"].map((item) => (
-                                        <li key={item.name} className="text-xs font-Roboto text-[#717171]">
-                                            {item.name}
-                                        </li>
-                                    ))
-                                    }
-                                </ul>
-                            </Disclosure.Panel>
+        <footer className="mx-auto md:px-24 ml-4  pt-8 mt-20  bg-transparent">
+            <div className="max-w-[1600px] relative h-80 mx-auto">
+                <div className="hidden md:grid grid-cols-12 gap-16 ">
+                    <div className="col-span-4 space-y-2 ">
+                        <div className=" mb-6">
+                            <img src="/LOGO PAYMENT.png" />
                         </div>
-                    )}
+                        <p className="text-justify font-normal text-xs leading-normal text-[#717171]">With Payment, we are bringing your favorite business to your Binance app. Go to Binance Marketplace with your existing Binance account and make a payment using Payment. Spending cryptocurrency has become a lot easier.</p>
+                    </div>
 
-                </Disclosure>
-
-                <Disclosure>
-                    {({ open }) => (
-                        <div className="my-3">
-                            <Disclosure.Button className="flex justify-between w-full">
-                                <h4 className="text-sm font-semibold">Service</h4>
-                                <ChevronDownIcon width={16} className={`${open?"":"rotate-90"}`}/>                            </Disclosure.Button>
-                            <Disclosure.Panel>
-                                <ul className="space-y-2 py-4">
-                                    {FooterNavigation["Service"].map((item) => (
-                                        <li key={item.name} className="text-xs font-Roboto text-[#717171]">
-                                            {item.name}
-                                        </li>
-                                    ))
-                                    }
-                                </ul>
-                            </Disclosure.Panel>
+                    <div className="md:flex mt-4 md:mt-0 col-span-6 md:space-x-2 space-y-4 md:space-y-0 justify-between">
+                        <div className="space-y-3 md:w-28">
+                            <h4 className="text-sm font-medium  mb-4">About us</h4>
+                            <ul className="space-y-2">
+                                {FooterNavigation["About us"].map((item) => (
+                                    <li key={item.name} className="text-xs text-[#2F88FF]">
+                                        {item.name}
+                                    </li>
+                                ))
+                                }
+                            </ul>
                         </div>
-                    )}
 
-                </Disclosure>
-
-                <Disclosure>
-                    {({ open }) => (
-                        <div className="my-3">
-                            <Disclosure.Button className="flex justify-between w-full">
-                                <h4 className="text-sm font-semibold">Support</h4>
-                                <ChevronDownIcon width={16} className={`${open?"":"rotate-90"}`}/>                            </Disclosure.Button>
-                            <Disclosure.Panel>
-                                <ul className="space-y-2 py-4">
-                                    {FooterNavigation["Support"].map((item) => (
-                                        <li key={item.name} className="text-xs font-Roboto text-[#717171]">
-                                            {item.name}
-                                        </li>
-                                    ))
-                                    }
-                                </ul>
-                            </Disclosure.Panel>
+                        <div className="space-y-3 md:w-28">
+                            <h4 className="text-sm font-medium mb-4">Service</h4>
+                            <ul className="space-y-2">
+                                {FooterNavigation["Service"].map((item) => (
+                                    <li key={item.name} className="text-xs text-[#2F88FF]">
+                                        {item.name}
+                                    </li>
+                                ))
+                                }
+                            </ul>
                         </div>
-                    )}
-                </Disclosure>
-            </div>
 
-            <div className="space-y-4 mt-4">
-                <h4 className="md:hidden font-semibold">Community</h4>
-                <div className="flex space-x-4">
-                    {Media.map((item) => (
-                        <img src={item} />
-                    ))}
+                        <div className="space-y-3 md:w-32">
+                            <h4 className="text-sm font-medium  mb-4">Support</h4>
+                            <ul className="space-y-2">
+                                {FooterNavigation["Support"].map((item) => (
+                                    <li key={item.name} className="text-xs text-[#2F88FF]">
+                                        {item.name}
+                                    </li>
+                                ))
+                                }
+                            </ul>
+                        </div>
+
+                        <div className="hidden md:block">
+                            <img className="absolute right-0 bottom-0" src="/footer/ellipse.png" />
+                        </div>
+
+                    </div>
+                </div>
+
+                {/* mobile */}
+                <div className="md:hidden">
+                    <div className="col-span-4 mb-4 ">
+                        <div className=" mb-6">
+                            <img src="/LOGO PAYMENT.png" />
+                        </div>
+                        <p className="text-justify font-normal text-xs leading-normal text-[#717171]">With Payment, we are bringing your favorite business to your Binance app. Go to Binance Marketplace with your existing Binance account and make a payment using Payment. Spending cryptocurrency has become a lot easier.</p>
+                    </div>
+                    <Disclosure>
+                        {({ open }) => (
+                            <div className="my-3">
+                                <Disclosure.Button className="flex justify-between w-full">
+                                    <h4 className="text-sm font-semibold" >About us</h4>
+                                    <ChevronDownIcon width={16} className={`${open ? "" : "rotate-90"}`} />
+                                </Disclosure.Button>
+                                <Disclosure.Panel>
+                                    <ul className="space-y-2 py-4">
+                                        {FooterNavigation["About us"].map((item) => (
+                                            <li key={item.name} className="text-xs font-Roboto text-[#717171]">
+                                                {item.name}
+                                            </li>
+                                        ))
+                                        }
+                                    </ul>
+                                </Disclosure.Panel>
+                            </div>
+                        )}
+
+                    </Disclosure>
+
+                    <Disclosure>
+                        {({ open }) => (
+                            <div className="my-3">
+                                <Disclosure.Button className="flex justify-between w-full">
+                                    <h4 className="text-sm font-semibold">Service</h4>
+                                    <ChevronDownIcon width={16} className={`${open ? "" : "rotate-90"}`} />                            </Disclosure.Button>
+                                <Disclosure.Panel>
+                                    <ul className="space-y-2 py-4">
+                                        {FooterNavigation["Service"].map((item) => (
+                                            <li key={item.name} className="text-xs font-Roboto text-[#717171]">
+                                                {item.name}
+                                            </li>
+                                        ))
+                                        }
+                                    </ul>
+                                </Disclosure.Panel>
+                            </div>
+                        )}
+
+                    </Disclosure>
+
+                    <Disclosure>
+                        {({ open }) => (
+                            <div className="my-3">
+                                <Disclosure.Button className="flex justify-between w-full">
+                                    <h4 className="text-sm font-semibold">Support</h4>
+                                    <ChevronDownIcon width={16} className={`${open ? "" : "rotate-90"}`} />                            </Disclosure.Button>
+                                <Disclosure.Panel>
+                                    <ul className="space-y-2 py-4">
+                                        {FooterNavigation["Support"].map((item) => (
+                                            <li key={item.name} className="text-xs font-Roboto text-[#717171]">
+                                                {item.name}
+                                            </li>
+                                        ))
+                                        }
+                                    </ul>
+                                </Disclosure.Panel>
+                            </div>
+                        )}
+                    </Disclosure>
+                </div>
+
+                <div className="space-y-4 mt-4 pb-20 md:pb-0">
+                    <h4 className="md:hidden font-semibold">Community</h4>
+                    <div className="flex space-x-4">
+                        {Media.map((item) => (
+                            <img src={item} />
+                        ))}
+                    </div>
                 </div>
             </div>
-
-            <div className="hidden md:block">
-                <img className="absolute right-0 bottom-0" src="/footer/ellipse.png" />
-            </div>
-
         </footer>
     )
 }
